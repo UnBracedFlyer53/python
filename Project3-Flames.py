46c909464447e9059b38fc53510bf4f58da12b03
Aditya Goyal <86954438+UnBracedFlyer53@users.noreply.github.com>
2022-06-09 01:24:08 +0530

    Add files via upload

---
+++ Project3-Flames.py
@@ -0,0 +1,22 @@
+print("\nthis program is created by Aditya Goyal.\n")
+print("FLAMES is a popular game named after the acronym: Friends, Lovers, Affectionate, Marriage, Enemies, Sibling. This game does not accurately predict whether or not an individual is right for you, but it can be fun to play this with your friends.\n")
+
+name1=input("Enter Name1: ")
+name2=input("Enter Name2: ")
+counter=0
+for i in name1:
+    if i in name2:
+        name1.replace(i,"")
+        name2.replace(i,"")
+count=len(name1)+len(name2)
+flames=["Friends","Lovers","Affectionate","Marriage","Enemies","Sibling"]
+while counter<5:
+    counter+=1
+    a=count%len(flames)
+
+    flames.remove(flames[a])
+
+for i in flames:
+    print(f"\n{name1} and {name2} will be {i}")
+
+print("\nThank You!\nAll The Best!")
